41c8f83520630c35eef95085a2668a541ab6b0f0
rubina <rubinakarki369@gmail.com>
2023-09-12 19:47:42 +0545

    [x] Separated Validation class and added __init__.py

--- generate-dummyf/helper_functions.py
+++ generate-dummyf/helper_functions.py
@@ -1,3 +1,4 @@
+from validator import Validation
 import random 
 from datetime import datetime
 import csv
@@ -6,35 +7,6 @@ import json
 import openpyxl
 from docx import Document
 from docx.shared import Pt
-
-class Validation:
-    def __init__(self,**kwargs):
-        self.arguments = kwargs
-        self.data_type_dict = {"file_size": int, "column_description" : dict, "font_size" : int}
-        self.allowed_data_types = ["int","str","boolean","datetime","float"]
-
-    def validate_args(self):
-        for key, value in self.arguments.items():
-            if not self.data_type_dict[key] == type(value):
-                raise Exception(f"Error: The value of '{key}' argument should be of {self.data_type_dict[key]} data type")
-
-    def validate_file_size_limit(self):
-        if not 0 <= self.arguments["file_size"] <= 5000000000: 
-            raise Exception(f"Error: The value of 'file_size' argument should be between 0 and 5GB")
-
-    def validate_column_description(self):
-        for key,value in self.arguments["column_description"].items():
-            if type(key) == str and type(value) == str:
-                if value in self.allowed_data_types:
-                    pass
-                else:
-                    raise Exception(f"Error: The values of 'column_description' dictionary should belong to the set of allowed data types i.e {self.allowed_data_types}")
-            else:
-                raise Exception(f"Error: The keys and values of 'column_description' dictionary should have key and value of string type")
-            
-    def validate_font_attributes(self):
-        if not 1 <= self.arguments["font_size"] <= 1683:
-            raise Exception(f"Error: The value of 'font_size' argument should be between 1 and 1683")
 
 def get_path_to_create_new_file(file_type):
     downloads_folder = f'{os.path.expanduser("~")}/Downloads/Dummy.{file_type}'
---
+++ generate-dummyf/validator.py
@@ -0,0 +1,28 @@
+class Validation:
+    def __init__(self,**kwargs):
+        self.arguments = kwargs
+        self.data_type_dict = {"file_size": int, "column_description" : dict, "font_size" : int}
+        self.allowed_data_types = ["int","str","boolean","datetime","float"]
+
+    def validate_args(self):
+        for key, value in self.arguments.items():
+            if not self.data_type_dict[key] == type(value):
+                raise Exception(f"Error: The value of '{key}' argument should be of {self.data_type_dict[key]} data type")
+
+    def validate_file_size_limit(self):
+        if not 0 <= self.arguments["file_size"] <= 5000000000: 
+            raise Exception(f"Error: The value of 'file_size' argument should be between 0 and 5GB")
+
+    def validate_column_description(self):
+        for key,value in self.arguments["column_description"].items():
+            if type(key) == str and type(value) == str:
+                if value in self.allowed_data_types:
+                    pass
+                else:
+                    raise Exception(f"Error: The values of 'column_description' dictionary should belong to the set of allowed data types i.e {self.allowed_data_types}")
+            else:
+                raise Exception(f"Error: The keys and values of 'column_description' dictionary should have key and value of string type")
+            
+    def validate_font_attributes(self):
+        if not 1 <= self.arguments["font_size"] <= 1683:
+            raise Exception(f"Error: The value of 'font_size' argument should be between 1 and 1683")
